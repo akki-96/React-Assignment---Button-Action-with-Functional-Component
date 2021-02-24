e07f05cf436863ef76c8cd6faba8a4b7e8c76947
akki-96 <75878738+akki-96@users.noreply.github.com>
2021-02-25 01:47:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,26 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [flag, useFlag] = useState(false);
+  fun = () => {
+    useFlag(true);
+  };
+  function fun1() {
+    if(flag){
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>
+    );
+    }
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={fun}>button</button>
+      {fun1()}
     </div>
   );
 }
-
 
 export default App;
