a40517cc5e7a8938d1ca35b18ef3f30189bde8e4
akki-96 <75878738+akki-96@users.noreply.github.com>
2021-03-04 09:43:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,59 @@
+// import React, {Component, useState} from "react";
+// import "./../styles/App.css";
+
+// function App() {
+//   const [flag, useFlag] = useState(false);
+//   fun = () => {
+//     useFlag(true);
+//   };
+//   function fun1() {
+//     if(flag){
+//     return (
+//       <p id="para">
+//         Hello, I've learnt to use the full-stack evaluation tool.This makes me so happy
+//       </p>
+//     );
+//     }
+//   }
+
+//   return (
+//     <div id="main">
+//       <button id="click" onClick={fun}>Button</button>
+//       {fun1()}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
 import React, {Component, useState} from "react";
-import "./../styles/App.css";
 
+//import "./../styles/App.css";
 function App() {
-  const [flag, useFlag] = useState(false);
-  fun = () => {
-    useFlag(true);
-  };
-  function fun1() {
-    if(flag){
-    return (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool.This makes me so happy
-      </p>
-    );
-    }
-  }
 
-  return (
-    <div id="main">
-      <button id="click" onClick={fun}>Button</button>
-      {fun1()}
-    </div>
-  );
+const [flag, useFlag] = useState(false);
+
+const fun = () => {
+
+useFlag(true);
+};
+
+return (
+
+<div id="main">
+
+<button id="click" onClick={fun}>Button</button>
+
+{ flag && <p id="para">
+
+Hello, I've learnt to use the full-stack evaluation tool.This makes me so happy </p>}
+
+</div>
+
+);
+
 }
 
 export default App;
